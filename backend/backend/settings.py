323576a4991d14b0100f617eae6bbb6fcf149cab
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-09-02 22:19:01 +0300

    Fix production settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,10 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['158.160.18.99', '127.0.0.1', 'localhost', 'be-thebest.ddns.net']
 
 # Application definition
 
